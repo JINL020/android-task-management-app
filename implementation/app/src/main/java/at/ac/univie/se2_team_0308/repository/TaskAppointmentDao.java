d79b67873c9e4e728143b10431c787cfa048dd36
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2022-11-24 17:38:28 +0100

    Add TaskAppointmentDao

---
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/repository/TaskAppointmentDao.java
@@ -0,0 +1,21 @@
+package at.ac.univie.se2_team_0308.repository;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+
+@Dao
+public interface TaskAppointmentDao {
+    @Insert
+    void insert(TaskAppointment task);
+
+    @Insert
+    void insertAll(TaskAppointment... tasks);
+
+    @Update
+    void update(TaskAppointment task);
+
+    @Delete
+    void delete(TaskAppointment task);
+}
