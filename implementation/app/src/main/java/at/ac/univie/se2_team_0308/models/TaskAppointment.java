23c5a8573ffa357b42c4e333e036faafa4e08e8c
Daria Lazepko <daria.lazepko@xooo.io>
2022-11-24 18:07:59 +0100

    created task types

---
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/models/TaskAppointment.java
@@ -0,0 +1,19 @@
+package at.ac.univie.se2_team_0308.models;
+import java.util.Date;
+
+public class TaskAppointment extends ATask {
+
+    private Date deadline;
+
+    public TaskAppointment(String taskName, String description){
+        super(taskName, description);
+    }
+
+    public Date getDeadline(){
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline){
+        this.deadline = deadline;
+    }
+}
---
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/models/TaskChecklist.java
@@ -0,0 +1,11 @@
+package at.ac.univie.se2_team_0308.models;
+
+public class TaskChecklist extends ATask {
+
+    public TaskChecklist(String taskName, String description){
+        super(taskName, description);
+    }
+
+    // TODO subtasks
+
+}
