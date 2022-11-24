5820a831b392090978f9e41a5c2ea693ac90cd7d
Jin-Jin Lee <a11913405@unet.univie.ac.at>
2022-11-24 17:38:05 +0100

    Add ECategory, ENotificationEvent, EPriority, EStatus

---
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/ECategory.java
@@ -0,0 +1,6 @@
+package at.ac.univie.se2_team_0308;
+
+public enum ECategory {
+    APPOINTMENT,
+    CHECKLIST
+}
---
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/ENotificationEvent.java
@@ -0,0 +1,8 @@
+package at.ac.univie.se2_team_0308;
+
+public enum ENotificationEvent {
+    CREATE,
+    DELETE,
+    UPDATE,
+    APPOINTMENT
+}
