30791f72e10ce2c7b197745e0558c60cff9e297e
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2022-11-24 19:36:21 +0100

    Add type converter for ECategory

--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/ECategoryTypeConverter.java
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/ECategoryTypeConverter.java
@@ -1,2 +1,22 @@
-package at.ac.univie.se2_team_0308.utils;public class ECategoryTypeConverter {
+package at.ac.univie.se2_team_0308.utils;
+
+import androidx.room.TypeConverter;
+
+import at.ac.univie.se2_team_0308.models.ECategory;
+
+public class ECategoryTypeConverter {
+    @TypeConverter
+    public String fromCategory(ECategory category){
+        return category.toString();
+    }
+
+    @TypeConverter
+    public ECategory toCategory(String category){
+        switch(category){
+            case "APPOINTMENT":
+                return ECategory.APPOINTMENT;
+            default:
+                return ECategory.CHECKLIST;
+        }
+    }
 }
