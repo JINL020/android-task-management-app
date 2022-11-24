75fffee01e0237389f6564407dcb595c04efe6a4
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2022-11-24 19:14:23 +0100

    Add type converter for EStatus

---
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/EStatusTypeConverter.java
@@ -0,0 +1,24 @@
+package at.ac.univie.se2_team_0308.utils;
+
+import androidx.room.TypeConverter;
+
+import at.ac.univie.se2_team_0308.models.EStatus;
+
+public class EStatusTypeConverter {
+    @TypeConverter
+    public String fromStatus(EStatus status){
+        return status.toString();
+    }
+
+    @TypeConverter
+    public EStatus toStatus(String status){
+        switch (status) {
+            case "IN_PROGRESS":
+                return EStatus.IN_PROGRESS;
+            case "NOT_STARTED":
+                return EStatus.NOT_STARTED;
+            default:
+                return EStatus.COMPLETED;
+        }
+    }
+}
