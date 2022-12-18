d0137167e6c9c819fa8d677de771920075833335
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2022-12-18 19:37:36 +0100

    Refactor TaskToXMLConverter

--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/export/TaskToXMLConverter.java
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/export/TaskToXMLConverter.java
@@ -1,2 +1,36 @@
-package at.ac.univie.se2_team_0308.utils.export;public class TaskToXMLConverter {
+package at.ac.univie.se2_team_0308.utils.export;
+
+import com.thoughtworks.xstream.XStream;
+
+import java.util.Date;
+import java.util.List;
+import at.ac.univie.se2_team_0308.models.ATask;
+import at.ac.univie.se2_team_0308.models.ECategory;
+import at.ac.univie.se2_team_0308.models.EPriority;
+import at.ac.univie.se2_team_0308.models.EStatus;
+
+public class TaskToXMLConverter implements ITaskConverter{
+    @Override
+    public String convertTasks(List<ATask> tasks, EFormat format) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<AllTasks>");
+        for(ATask eachTask : tasks){
+            String xml = convertTask(eachTask);
+            stringBuilder.append(xml);
+        }
+        stringBuilder.append("<AllTasks>");
+        return stringBuilder.toString();
+    }
+    private String convertTask(ATask task){
+        XStream xstream = new XStream();
+//        xstream.alias("attachment", Attachment.class);
+        xstream.alias("category", ECategory.class);
+//        xstream.alias("date", Date);
+        xstream.alias("status", EStatus.class);
+        xstream.alias("priority", EPriority.class);
+//        xstream.alias("notificationService", NotificationService.class);
+//        xstream.alias();
+        String xml = xstream.toXML(task);
+        return xml;
+    }
 }
