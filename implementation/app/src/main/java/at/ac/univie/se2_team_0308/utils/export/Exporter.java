02c02f2f987a82fc8529f3207550a35ca6068384
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2022-12-18 18:15:12 +0100

    ITaskConverter

--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/export/Exporter.java
+++
@@ -1,2 +0,0 @@
-package at.ac.univie.se2_team_0308.utils.export;public class Exporter {
-}
--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/export/ITaskConverter.java
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/export/ITaskConverter.java
@@ -1,2 +1,9 @@
-package at.ac.univie.se2_team_0308.utils.export;public interface ITaskConverter {
+package at.ac.univie.se2_team_0308.utils.export;
+
+import java.util.List;
+
+import at.ac.univie.se2_team_0308.models.ATask;
+
+public interface ITaskConverter {
+    public String convertTasks(List<ATask> tasks, EFormat format);
 }
