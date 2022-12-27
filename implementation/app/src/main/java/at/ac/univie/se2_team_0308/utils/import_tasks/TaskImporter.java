cadf2bd698b43d6ba53c2b55a52c233a468aa02f
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2022-12-27 18:01:29 +0100

    Add interface to import tasks from files

--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/import_tasks/TaskImporter.java
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/import_tasks/TaskImporter.java
@@ -1,4 +1,16 @@
 package at.ac.univie.se2_team_0308.utils.import_tasks;
 
-public interface Importer {
+import android.util.Pair;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import at.ac.univie.se2_team_0308.models.TaskAppointment;
+import at.ac.univie.se2_team_0308.models.TaskChecklist;
+
+public interface TaskImporter {
+    public Pair<TaskAppointment, TaskChecklist> importTasks() throws IOException, ParserConfigurationException, SAXException;
 }
