eed8ccaa678e5b92bbb2ed1c00a56b5cb7b39c0a
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2022-12-27 17:22:21 +0100

    Retrieve individual strings containing tasks

--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/import_tasks/JsonTaskRetriever.java
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/utils/import_tasks/JsonTaskRetriever.java
@@ -1,9 +1,24 @@
 package at.ac.univie.se2_team_0308.utils.import_tasks;
 
+import android.util.Log;
 import android.util.Pair;
+import java.util.List;
 
-public class JsonTaskInterpreter {
-    public Pair<String, String> getTasks(String fileContent){
+public class JsonTaskRetriever {
+    private static final String TAG = "JsonTaskRetriever";
+    private String fileContent;
 
+    public JsonTaskRetriever(String fileContent){
+        this.fileContent = fileContent;
+    }
+    public Pair<List<String>, List<String>> getTasks(){
+        Log.d(TAG, "Retrieve individual strings containing tasks");
+        String regexAppointment = "\\{.*\"category\":\"APPOINTMENT\".*\\}";
+        String regexChecklist = "\\{.*\"category\":\"CHECKLIST\".*\\}";
+
+        List<String> taskAppointmentStrings = MatchRetriever.retrieveTasks(regexAppointment, fileContent);
+        List<String> taskChecklistStrings = MatchRetriever.retrieveTasks(regexChecklist, fileContent);
+
+        return new Pair<>(taskAppointmentStrings, taskChecklistStrings);
     }
 }
