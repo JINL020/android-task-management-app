7340a631a6f5e8039be83f32fba077391b0a38f5
Jin-Jin Lee <a11913405@unet.univie.ac.at>
2023-01-04 21:24:15 +0100

    added listeners to settings checkboxes and building observer

--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/views/SettingsFragment.java
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/views/SettingsFragment.java
@@ -12,15 +12,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import at.ac.univie.se2_team_0308.databinding.FragmentSettingsBinding;
+import at.ac.univie.se2_team_0308.utils.notifications.BasicNotifier;
+import at.ac.univie.se2_team_0308.utils.notifications.IObserver;
+import at.ac.univie.se2_team_0308.utils.notifications.LoggerCore;
+import at.ac.univie.se2_team_0308.utils.notifications.PopupNotifier;
 
 public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
+
     private CheckBox onCreatePopupCheckBox;
     private CheckBox onCreateBasicCheckBox;
     private CheckBox onUpdatePopupCheckBox;
     private CheckBox onUpdateBasicCheckBox;
     private CheckBox onDeletePopupCheckBox;
     private CheckBox onDeleteBasicCheckBox;
+
+    private IObserver onCreateNotifier = new LoggerCore();
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
@@ -31,12 +38,36 @@ public class SettingsFragment extends Fragment {
         onCreatePopupCheckBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(((CompoundButton) view).isChecked()){
-                    Log.d("checkbox", "checked");
-                    System.out.println("Checked");
-                } else {
-                    Log.d("checkbox", "unchecked");
-                }
+                boolean onCreatePopup = onCreatePopupCheckBox.isChecked();
+                boolean onCreateBasic = onCreateBasicCheckBox.isChecked();
+                IObserver onCreateNotifier = buildNotifier(onCreatePopup, onCreateBasic);
+            }
+        });
+
+        onCreateBasicCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean onCreatePopup = onCreatePopupCheckBox.isChecked();
+                boolean onCreateBasic = onCreateBasicCheckBox.isChecked();
+                IObserver onCreateNotifier = buildNotifier(onCreatePopup, onCreateBasic);
+            }
+        });
+
+        onUpdatePopupCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean onCreatePopup = onUpdatePopupCheckBox.isChecked();
+                boolean onCreateBasic = onUpdateBasicCheckBox.isChecked();
+                IObserver onUpdateNotifier = buildNotifier(onCreatePopup, onCreateBasic);
+            }
+        });
+
+        onUpdateBasicCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean onCreatePopup = onUpdatePopupCheckBox.isChecked();
+                boolean onCreateBasic = onUpdateBasicCheckBox.isChecked();
+                IObserver onUpdateNotifier = buildNotifier(onCreatePopup, onCreateBasic);
             }
         });
 
@@ -57,4 +88,17 @@ public class SettingsFragment extends Fragment {
         onDeletePopupCheckBox = binding.checkBoxOnDeletePopup;
         onDeleteBasicCheckBox = binding.checkBoxOnDeleteBasic;
     }
+
+    private IObserver buildNotifier(boolean popup, boolean basic) {
+        IObserver notifier = new LoggerCore();
+        if (popup) {
+            notifier = new PopupNotifier(notifier);
+        }
+        if (basic) {
+            notifier = new BasicNotifier(notifier);
+        }
+        Log.d("settings", notifier.getNotifierType().toString());
+        return notifier;
+    }
+
 }
