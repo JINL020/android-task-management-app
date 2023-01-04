cbb8daf92100012b73a14341557479ab40744ed0
Jin-Jin Lee <a11913405@unet.univie.ac.at>
2023-01-04 20:48:51 +0100

    finished settings layout, init views infragment

--- implementation/app/src/main/java/at/ac/univie/se2_team_0308/views/SettingsFragment.java
+++ implementation/app/src/main/java/at/ac/univie/se2_team_0308/views/SettingsFragment.java
@@ -1,9 +1,12 @@
 package at.ac.univie.se2_team_0308.views;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,10 +15,30 @@ import at.ac.univie.se2_team_0308.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
+    private CheckBox onCreatePopupCheckBox;
+    private CheckBox onCreateBasicCheckBox;
+    private CheckBox onUpdatePopupCheckBox;
+    private CheckBox onUpdateBasicCheckBox;
+    private CheckBox onDeletePopupCheckBox;
+    private CheckBox onDeleteBasicCheckBox;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        initViews();
+
+        onCreatePopupCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(((CompoundButton) view).isChecked()){
+                    Log.d("checkbox", "checked");
+                    System.out.println("Checked");
+                } else {
+                    Log.d("checkbox", "unchecked");
+                }
+            }
+        });
 
         return root;
     }
@@ -24,5 +47,14 @@ public class SettingsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void initViews() {
+        onCreatePopupCheckBox = binding.checkBoxOnCreatePopup;
+        onCreateBasicCheckBox = binding.checkBoxOnCreateBasic;
+        onUpdatePopupCheckBox = binding.checkBoxOnUpdatePopup;
+        onUpdateBasicCheckBox = binding.checkBoxOnUpdateBasic;
+        onDeletePopupCheckBox = binding.checkBoxOnDeletePopup;
+        onDeleteBasicCheckBox = binding.checkBoxOnDeleteBasic;
     }
 }
