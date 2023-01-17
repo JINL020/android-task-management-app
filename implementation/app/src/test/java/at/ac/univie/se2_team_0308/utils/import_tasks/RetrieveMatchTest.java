2937f71cbb8491de84377bc21ee94dcd4215ff45
Miruna-Diana Jarda <a11921801@unet.univie.ac.at>
2023-01-17 20:18:53 +0100

    Test retrieve matcher

--- implementation/app/src/test/java/at/ac/univie/se2_team_0308/utils/import_tasks/RetrieveMatchTest.java
+++ implementation/app/src/test/java/at/ac/univie/se2_team_0308/utils/import_tasks/RetrieveMatchTest.java
@@ -1,50 +1,92 @@
 package at.ac.univie.se2_team_0308.utils.import_tasks;
 
+import static org.junit.Assert.assertEquals;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
 import java.util.List;
 
-public class RetrieveMatchTest extends TestCase {
+public class RetrieveMatchTest {
     @Test
-    public void retrieveMatch_withDotallTrue_shouldReturnAllMatches() {
-        String regex = "\\d+";
-        String text = "The numbers are 42, 56 and 78.";
+    public void dotallTrue_twoMatchesConsideredAcrossMultipleLines() {
+        String regex = "<at.ac.univie.se2__team__0308.models.TaskChecklist>.*?</at.ac.univie.se2__team__0308.models.TaskChecklist>";
+        String start = "<AllTasks>";
+        String task1 =
+                "<at.ac.univie.se2__team__0308.models.TaskChecklist>\n" +
+                "  <id>1</id>\n" +
+                "  <taskName>taskName</taskName>\n" +
+                "  <description>description</description>\n" +
+                "  <priority>LOW</priority>\n" +
+                "  <status>NOT_STARTED</status>\n" +
+                "  <isSelected>false</isSelected>\n" +
+                "  <category>CHECKLIST</category>\n" +
+                "  <isHidden>false</isHidden>\n" +
+                "  <sketchData></sketchData>\n" +
+                "  <attachments/>\n" +
+                "  <taskColor></taskColor>\n" +
+                "  <creationDate>3920-06-11 22:00:00.0 UTC</creationDate>\n" +
+                "  <subtasks/>\n" +
+                "</at.ac.univie.se2__team__0308.models.TaskChecklist>";
+        String task2 =
+                "<at.ac.univie.se2__team__0308.models.TaskChecklist>\n" +
+                "  <id>0</id>\n" +
+                "  <taskName>taskName</taskName>\n" +
+                "  <description>description</description>\n" +
+                "  <priority>LOW</priority>\n" +
+                "  <status>NOT_STARTED</status>\n" +
+                "  <isSelected>false</isSelected>\n" +
+                "  <category>CHECKLIST</category>\n" +
+                "  <isHidden>false</isHidden>\n" +
+                "  <sketchData></sketchData>\n" +
+                "  <attachments/>\n" +
+                "  <taskColor></taskColor>\n" +
+                "  <creationDate>3920-06-11 22:00:00.0 UTC</creationDate>\n" +
+                "  <subtasks/>\n" +
+                "</at.ac.univie.se2__team__0308.models.TaskChecklist>";
+        String end = "</AllTasks>";
+
+        String text = start + task1 + "\n" + task2 + "\n" + end;
         boolean dotall = true;
-
         List<String> matches = MatchRetriever.retrieveMatch(regex, text, dotall);
 
-        assertEquals(3, matches.size());
-        assertEquals("42", matches.get(0));
-        assertEquals("56", matches.get(1));
-        assertEquals("78", matches.get(2));
+        assertEquals(2, matches.size());
+        assertEquals(task1, matches.get(0));
+        assertEquals(task2, matches.get(1));
     }
 
     @Test
-    public void retrieveMatch_withDotallFalse_shouldReturnAllMatches() {
-        String regex = "\\d+";
-        String text = "The numbers are 42, 56 and 78.";
-        boolean dotall = false;
+    public void dotallFalse_twoMatchesConsideredAcrossOneLine() {
+        String regex = "\\{.*\"category\":\"APPOINTMENT\".*\\}";
+        String start = "{\"AllTasks\": [\n";
+        String task1 = "{\"deadline\":\"Jan 17, 2023 2:33:00 PM\",\"attachments\":[],\"category\":\"APPOINTMENT\",\"creationDate\":\"Jan 17, 2023 2:33:24 PM\",\"description\":\"\",\"id\":1,\"isHidden\":false,\"isSelected\":true,\"priority\":\"LOW\",\"sketchData\":[],\"status\":\"NOT_STARTED\",\"taskColor\":\"#E1E1E1\",\"taskName\":\"dasd\"}";
+        String task2 = "{\"deadline\":\"Jan 17, 2023 7:52:00 PM\",\"attachments\":[],\"category\":\"APPOINTMENT\",\"creationDate\":\"Jan 17, 2023 7:52:32 PM\",\"description\":\"\",\"id\":2,\"isHidden\":false,\"isSelected\":true,\"priority\":\"LOW\",\"sketchData\":[],\"status\":\"NOT_STARTED\",\"taskColor\":\"#E1E1E1\",\"taskName\":\"dsfs\"}";
+        String end = "]}";
 
+        String text = start + task1 + ",\n" + task2 + "\n" + end;
+        boolean dotall = false;
         List<String> matches = MatchRetriever.retrieveMatch(regex, text, dotall);
 
-        assertEquals(3, matches.size());
-        assertEquals("42", matches.get(0));
-        assertEquals("56", matches.get(1));
-        assertEquals("78", matches.get(2));
+        assertEquals(2, matches.size());
+        assertEquals(task1, matches.get(0));
+        assertEquals(task2, matches.get(1));
     }
 
     @Test
-    public void retrieveMatch_withInvalidRegex_shouldReturnEmptyList() {
-        String regex = "[a-z";
-        String text = "The numbers are 42, 56 and 78.";
-        boolean dotall = false;
+    public void dotallFalse_oneMatchConsideredAcrossOneLine() {
+        String regex = "\\{.*\"category\":\"APPOINTMENT\".*\\}";
+        String start = "{\"AllTasks\": [\n";
+        String task1 = "{\"deadline\":\"Jan 17, 2023 2:33:00 PM\",\"attachments\":[],\"category\":\"APPOINTMENT\",\"creationDate\":\"Jan 17, 2023 2:33:24 PM\",\"description\":\"\",\"id\":1,\"isHidden\":false,\"isSelected\":true,\"priority\":\"LOW\",\"sketchData\":[],\"status\":\"NOT_STARTED\",\"taskColor\":\"#E1E1E1\",\"taskName\":\"dasd\"}";
+        String task2 = "{\"deadline\":\"Jan 17, 2023 7:52:00 PM\",\"attachments\":[],\"category\":\"APPOINTMENT\",\"creationDate\":\"Jan 17, 2023 7:52:32 PM\",\"description\":\"\",\"id\":2,\"isHidden\":false,\"isSelected\":true,\"priority\":\"LOW\",\"sketchData\":[],\"status\":\"NOT_STARTED\",\"taskColor\":\"#E1E1E1\",\"taskName\":\"dsfs\"}";
+        String end = "]}";
+        String expected = start + task1 + ",\n" + task2 + "\n" + end;
 
-        List<String> matches = MatchRetriever.retrieveMatch(regex, text, dotall);
+        boolean dotall = true;
+        List<String> matches = MatchRetriever.retrieveMatch(regex, expected, dotall);
+        String actual = matches.get(0);
 
-        assertEquals(0, matches.size());
+        assertEquals(1, matches.size());
+        assertEquals(expected, actual);
     }
-
-
 }
